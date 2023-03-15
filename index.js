e298cede780627dc7630d231ea33e2dd049c05f5
Natan Journo <natanjourno@MBP-de-Natan.lan>
2023-03-15 15:36:19 +0100

    ✅ Change the back-end (javascript) with the right rules and allowing you to play directly on a web page

--- index.js
+++ index.js
@@ -1,139 +1,167 @@
-// Promise that reads a line of input from the user in the console
-const readline = require('readline');
+let diceArr = [];
+let score = 0;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function getUserInput(question) {
-    return new Promise(resolve => {
-        rl.question(question, answer => {
-            resolve(answer);
-        });
-    });
+// Initialization of dice in objects
+function initializeDice() {
+    for (i = 0; i < 6; i++) {
+        diceArr[i] = {};
+        diceArr[i].id = "die" + String(i + 1);
+        diceArr[i].value = i + 1;
+    }
 }
 
-// Generating random number between 1 and 6
-function rollDie() {
-    return Math.floor(Math.random() * 6) + 1;
+// Rolling dice
+function rollDice() {
+    for (let i = 0; i < 6; i++) {
+        diceArr[i].value = Math.floor(Math.random() * 6 + 1);
+    }
+    updateDiceImg();
+    getScore();
 }
 
-async function playFarkle(playerName) {
+// Change dice image when rolling dice
+function updateDiceImg() {
+    let diceImage;
+    for (let i = 0; i < 6; i++) {
+        diceImage = "img/" + diceArr[i].value + ".png";
+        document.getElementById(diceArr[i].id).setAttribute("src", diceImage);
+    }
+}
 
-    console.log(`Tour de ${playerName}`);
-    console.log('Jouons à Farkle !');
-    console.log('------------------');
+/*
+[
+  { id: 'die1', value: 2 },
+  { id: 'die2', value: 5 },
+  { id: 'die3', value: 3 },
+  { id: 'die4', value: 5 },
+  { id: 'die5', value: 2 },
+  { id: 'die6', value: 1 }
+]
 
+to [1, 2, 2, 0, 0, 2] with getDiceAmounts
+
+*/
+function getDiceAmounts() {
+    let valueArr = [0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < 6; i++) {
+        valueArr[diceArr[i].value - 1]++;
+    }
+    return valueArr;
+}
+
+// if there a Farkle then the turn goes to the next player
+function checkForFarkle() {
+    let valueArr = getDiceAmounts();
+    for (let i = 0; i < 6; i++) {
+        if (valueArr[i] >= 3) {
+            return;
+        } else if (i == 0 && valueArr[i] > 0) {
+            return;
+        } else if (i == 4 && valueArr[i] > 0) {
+            return;
+        }
+  }
+
+  alert("FARKLE! Votre tour est fini !");
+}
+
+function getScore(losingDice) {
+    let valueArr = getDiceAmounts();
     let score = 0;
-    let round = 1;
-    let consecutiveNonScoringRounds = 0;
 
-    // Maximum of 10 rounds for more fun !!
-    while (round <= 10) {
-        console.log(`Manche ${round}`);
-        console.log(`Score actuel: ${score}`);
+    // Filter out losing dice for future feature !!
+    if (losingDice && losingDice.length > 0) {
+        valueArr = valueArr.filter((_, index) => !losingDice.includes(index + 1));
+    }
 
-        // Table for the future result
-        let dice = [0, 0, 0, 0, 0];
-
-        let numDice = 5;
-
-        // There are still dice to roll
-        while (numDice > 0) {
-            let roll = rollDie();
-            dice[numDice - 1] = roll; // in the table
-            console.log(`Dé ${numDice}: ${roll}`);
-            numDice--;
-        }
-
-        let roundScore = 0;
-
-        // Research of 1 and 5
-        let ones = dice.filter(d => d === 1).length;
-        let fives = dice.filter(d => d === 5).length;
-
-        if (ones === 0 && fives === 0) {
-            consecutiveNonScoringRounds++;
-            console.log('Aucun 1 ni 5, Farkle !');
-            roundScore = 0;
-
-            // Adding a rule if 3 Farkle then end of player’s turn
-            if (consecutiveNonScoringRounds === 3) {
-                console.log(`Trois manches consécutives sans points pour ${playerName}, le tour passe à l'autre joueur.`);
-                consecutiveNonScoringRounds = 0;
-                break;
+    // Calculate score
+    for (let i = 0; i < 6; i++) {
+        if (i == 0 && valueArr[i] >= 3 && valueArr[i] < 6) {
+            score += 1000;
+            if (valueArr[i] - 3 == 1) {
+                score += 100;
+            } else if (valueArr[i] - 3 == 2) {
+                score += 200;
             }
 
-        } else {
-            consecutiveNonScoringRounds = 0;
-            roundScore += ones * 100;
-            roundScore += fives * 50;
+        } else if (i == 0 && valueArr[i] >= 6) {
+            score += 2000;
 
-            // Search for three dice of the same value
-            for (let i = 1; i <= 6; i++) {
-                let count = dice.filter(d => d === i).length;
+        } else if (i == 0) {
+            score += valueArr[i] * 100;
 
-                if (count === 3) {
-                    // 1000 points awarded if 3 times of 1
-                    if (i === 1) {
-                        roundScore += 1000;
-                    } else {
-                        roundScore += i * 100;
-                    }
-
-                break;
-                }
+        } else if (i == 4 && valueArr[i] >= 3 && valueArr[i] < 6) {
+            score += 500;
+            if (valueArr[i] - 3 == 1) {
+                score += 50;
+            } else if (valueArr[i] - 3 == 2) {
+                score += 100;
             }
-        }
 
-        console.log(`Score de la manche: ${roundScore}`);
-
-        let response = await getUserInput('Voulez-vous continuer ? (o/n) ');
-        // In case player writes in UPPERCASE or simple presses ENTER
-        if (response.toLowerCase() === 'o' || response === '') {
-            score += roundScore;
-            round++;
-        } else {
-            break;
+        } else if (i == 4 && valueArr[i] >= 6) {
+            score += 1000;
+        } else if (i == 4) {
+            score += valueArr[i] * 50;
+        } else if (i == 1 && valueArr[i] == 3) {
+            score += 200;
+        } else if (i == 2 && valueArr[i] == 3) {
+            score += 300;
+        } else if (i == 3 && valueArr[i] == 3) {
+            score += 400;
+        } else if (i == 5 && valueArr[i] == 3) {
+            score += 600;
+        } else if (i == 1 && valueArr[i] == 6) {
+            score += 400;
+        } else if (i == 2 && valueArr[i] == 6) {
+            score += 600;
+        } else if (i == 3 && valueArr[i] == 6) {
+            score += 800;
+        } else if (i == 5 && valueArr[i] == 6) {
+            score += 1200;
         }
     }
 
-    console.log('------------------');
-    console.log(`Le score de ${playerName} est de ${score} points.`);
-    console.log('------------------');
-    
+    document.getElementById("row-score").innerHTML = score;
+
+    if (score == 0) {
+        alert("FARKLE! Votre tour est fini !");
+    }
+
     return score;
 }
 
-async function main() {
-    console.log('Bienvenue à Farkle !');
-    console.log('------------------');
-  
-    let playAgain = true;
-  
-    // As long as users want to play and so playAgain === TRUE
-    while (playAgain) {
-      let scores = {};
-  
-      let player1 = await getUserInput('Nom du premier joueur : ');
-      scores[player1] = await playFarkle(player1);
-  
-      let player2 = await getUserInput('Nom du deuxième joueur : ');
-      scores[player2] = await playFarkle(player2);
-  
-      console.log('------------------');
-      console.log('Le jeu est terminé !');
-      console.log(`${player1} a marqué ${scores[player1]} points.`);
-      console.log(`${player2} a marqué ${scores[player2]} points.`);
-      console.log(`${scores[player1] > scores[player2] ? player1 : player2} a gagné !`);
-  
-      let response = await getUserInput('Voulez-vous jouer à nouveau ? (o/n) ');
+// The reset function will be called at the end of the game
+function resetGame() {
+    diceArr = [];
+    score = 0;
+    initializeDice();
+    updateDiceImg();
+    document.getElementById("row-score").innerHTML = score;
+    document.getElementById("player1-score").innerHTML = 0;
+    document.getElementById("player2-score").innerHTML = 0;
+    document.getElementById("turn-number").innerHTML = 1;
+}
 
-      if (response.toLowerCase() === 'n') {
-        playAgain = false;
-      }
+function bankScore() {
+
+    let currentPlayer = (document.getElementById("turn-number").innerHTML % 2) + 1;
+    let currentScore = getScore(); console.log("Player " + currentPlayer + " a " + currentScore + " points");
+
+    let playerScoreElement = document.getElementById("player" + currentPlayer + "-score");
+    let playerScore = parseInt(playerScoreElement.innerHTML);
+    playerScore += currentScore;
+    playerScoreElement.innerHTML = playerScore;
+
+    let turnNumberElement = document.getElementById("turn-number");
+    let turnNumber = parseInt(turnNumberElement.innerHTML);
+    turnNumber += 1;
+    turnNumberElement.innerHTML = turnNumber;
+
+    // Check for winner
+    if (playerScore >= 3000) {
+        let winner = "Player " + currentPlayer;
+        alert(winner + " a gagné ! Voulez-vous rejouer ?");
+        resetGame();
     }
-  }
-  
-  main();
+
+}
